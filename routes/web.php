3d0ea8fdc76007ad40140e3a84fb324d9e112ba0
Abdrahman Moussa <maloom7>
2022-12-28 18:38:00 +0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\RoleController;
+
+use App\Http\Controllers\UserController;
+
+use App\Http\Controllers\ProductController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,4 +29,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
+});
